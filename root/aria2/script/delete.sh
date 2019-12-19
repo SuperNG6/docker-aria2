f473dc7b3203e9f6745c36e67086fe1e6aa8a1f0
SuperNG6 <sleele@gmail.com>
2019-12-19 11:02:00 +0800

    update script

--- root/aria2/script/delete.sh
+++ root/aria2/script/delete.sh
@@ -7,22 +7,22 @@ rdp=${filepath#${downloadpath}/}
 path=${downloadpath}/${rdp%%/*}
 
 if [ $2 -eq 0 ]
-    then
-        exit 0
+	then
+		exit 0
 elif [ "$path" = "$filepath" ] && [ $2 -eq 1 ]
-    then
-    [ -e "$filepath".aria2 ] && rm -vf "$filepath".aria2
-    rm -vf "$filepath"
-    exit 0
-elif [ "$path" != "$filepath" ] && [ $2 -gt 1 ]
-    then
-    [ -e "$path".aria2 ] && rm -vf "$path".aria2
-    rm -vrf "$path"
-    exit 0
-elif [ "$path" != "$filepath" ] && [ $2 -eq 1 ]
-    then
-    [ -e "$filepath".aria2 ] && rm -vf "$filepath".aria2
-    rm -vf "$filepath"
-    find "${downloadpath}" ! -path "${downloadpath}" -depth -type d -empty -exec rm -vrf {} \;
-    exit 0
+	then
+	rm -vf "$filepath".aria2
+	rm -vf "$filepath"
+	exit 0
+elif [ "$path" != "$filepath" ] && [ -e "$filepath".aria2 ]
+	then
+	rm -vf "$filepath".aria2
+	rm -vf "$filepath"
+	rmdir -v "${filepath%/*}"
+	exit 0
+elif [ "$path" != "$filepath" ] && [ -e "$path".aria2 ]
+	then
+	rm -vf "$path".aria2
+	rm -vrf "$path"
+	exit 0
 fi
