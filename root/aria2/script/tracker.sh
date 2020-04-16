fa06128b0a3b0648f30355ceff2b171918ee543a
SuperNG6 <sleele@gmail.com>
2020-04-16 17:30:57 +0800

    使用jsdelivr cdn加速下载trackers，但时会出现缓存导致的不是最新版本

--- root/aria2/script/tracker.sh
+++ root/aria2/script/tracker.sh
@@ -7,7 +7,7 @@ aria2_conf="/config/aria2.conf"
 # https://github.com/ngosang/trackerslist
 #tracker=$(wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk NF | sed ":a;N;s/\n/,/g;ta")
 # https://github.com/XIU2/TrackersListCollection
-tracker=$(wget -qO- https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk NF | sed ":a;N;s/\n/,/g;ta")
+tracker=$(wget -qO- https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection/all.txt | awk NF | sed ":a;N;s/\n/,/g;ta")
 [ -z $tracker ] && echo -e "
 $ERROR Unable to get trackers, network failure or invalid links." && exit 1
 echo -e "
