3cc5b571cf8a3b4233c3a1946ad7bd3cc61db38e
SuperNG6 <sleele@gmail.com>
2021-01-24 20:49:06 +0800

    update scripts

--- root/aria2/script/completed.sh
+++ root/aria2/script/completed.sh
@@ -6,11 +6,15 @@
 FILE_PATH=$3
 FILE_NUM=$2
 
+GET_BASE_PATH
+COMPLETED_PATH
+GET_PATH
+
 if [ "${FILE_NUM}" -eq 0 ]; then
     exit 0
+elif [ "${GET_PATH_INFO}" = "error" ]; then
+    echo -e "$(DATE_TIME) ${ERROR} GET TASK PATH ERROR!"
+    exit 1
 else
-    GET_BASE_PATH
-    COMPLETED_PATH
-    GET_PATH
     MOVE_FILE
 fi
--- root/aria2/script/stop.sh
+++ root/aria2/script/stop.sh
@@ -6,26 +6,24 @@
 FILE_PATH=$3
 FILE_NUM=$2
 
+GET_BASE_PATH
+RECYCLE_PATH
+GET_PATH
+
 if [ "${FILE_NUM}" -eq 0 ]; then
     exit 0
-else
-    if [ "${RMTASK}" = "recycle" ]; then
-        GET_BASE_PATH
-        RECYCLE_PATH
-        GET_PATH
-        MOVE_RECYCLE
-        RM_ARIA2
-        exit 0
-    elif [ "${RMTASK}" = "delete" ]; then
-        GET_BASE_PATH
-        GET_PATH
-        DELETE_FILE
-        RM_ARIA2
-        exit 0
-    elif [ "${RMTASK}" = "rmaria" ]; then
-        GET_BASE_PATH
-        GET_PATH
-        RM_ARIA2
-        exit 0
-    fi
+elif [ "${GET_PATH_INFO}" = "error" ]; then
+    echo -e "$(DATE_TIME) ${ERROR} GET TASK PATH ERROR!"
+    exit 1
+elif [ "${RMTASK}" = "recycle" ]; then
+    MOVE_RECYCLE
+    RM_ARIA2
+    exit 0
+elif [ "${RMTASK}" = "delete" ]; then
+    DELETE_FILE
+    RM_ARIA2
+    exit 0
+elif [ "${RMTASK}" = "rmaria" ]; then
+    RM_ARIA2
+    exit 0
 fi
