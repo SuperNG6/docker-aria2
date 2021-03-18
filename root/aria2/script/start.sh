6f76020d99b01b5305e428b52d02de0d0bd956b7
SuperNG6 <sleele@gmail.com>
2021-03-18 19:48:50 +0800

    控制台任务信息显示支持中文

--- root/aria2/script/start.sh
+++ root/aria2/script/start.sh
@@ -15,15 +15,15 @@ GET_FINAL_PATH
 
 START() {
     # aria2开始任务时，单文件不会传递`FILE_PATH`，磁力`FILE_NUM`为0；`TASK_STATUS`为`error`时，多为存在`.aria2控制文件`,任务文件已存在
-    # 判断`COMPLETED_DIR`是否存在已完成文件夹，如果有，则通过rpc删除该任务，同时删除任务文件夹和控制文件
+    # 判断`COMPLETED_DIR`是否存在已完成任务，如果有，则通过rpc删除该任务，同时删除该任务文件和控制文件
     if [ "${FILE_NUM}" -eq 0 ] || [ -z "${FILE_PATH}" ]; then
         exit 0
     elif [ "${GET_PATH_INFO}" = "error" ]; then
         echo -e "$(DATE_TIME) ${ERROR} GID:${TASK_GID} GET TASK PATH ERROR!"
         exit 1
     elif [ -d "${COMPLETED_DIR}" ] && [ "${TASK_STATUS}" != "error" ]; then
-        echo -e "$(DATE_TIME) ${WARRING} Repeat task ${LIGHT_GREEN_FONT_PREFIX}${COMPLETED_DIR}${FONT_COLOR_SUFFIX}"
-        echo -e "$(DATE_TIME) ${WARRING} Remove task ${LIGHT_GREEN_FONT_PREFIX}${SOURCE_PATH}${FONT_COLOR_SUFFIX}"
+        echo -e "$(DATE_TIME) ${WARRING} 发现目标文件夹已存在当前任务 ${LIGHT_GREEN_FONT_PREFIX}${COMPLETED_DIR}${FONT_COLOR_SUFFIX}"
+        echo -e "$(DATE_TIME) ${WARRING} 正在删除该任务，并清除相关文件... ${LIGHT_GREEN_FONT_PREFIX}${SOURCE_PATH}${FONT_COLOR_SUFFIX}"
         RM_ARIA2
         rm -rf "${SOURCE_PATH}"
         REMOVE_REPEAT_TASK
