e638d70c0031b6819871598d5b0e0a1cb31a031b
SuperNG6 <sleele@gmail.com>
2021-10-09 21:46:29 +0800

    `-e CTU=`支持更新conf trackers
    `-e CTU=`支持更新conf trackers
    
    Update tracker.sh

--- root/aria2/script/tracker.sh
+++ root/aria2/script/tracker.sh
@@ -1,24 +1,71 @@
 #!/usr/bin/env bash
+#
+# https://github.com/P3TERX/aria2.conf
+# File name：tracker.sh
+# Description: Get BT trackers and add to Aria2
 
-INFO="[\033[32mINFO\033[0m]"
-ERROR="[\033[31mERROR\033[0m]"
+RED_FONT_PREFIX="\033[31m"
+GREEN_FONT_PREFIX="\033[32m"
+YELLOW_FONT_PREFIX="\033[1;33m"
+LIGHT_PURPLE_FONT_PREFIX="\033[1;35m"
+FONT_COLOR_SUFFIX="\033[0m"
+INFO="[${GREEN_FONT_PREFIX}INFO${FONT_COLOR_SUFFIX}]"
+ERROR="[${RED_FONT_PREFIX}ERROR${FONT_COLOR_SUFFIX}]"
+ARIA2_CONF=${1:-aria2.conf}
+DOWNLOADER="curl -fsSL --connect-timeout 3 --max-time 3 --retry 2"
+SCRIPT_CONF="/config/setting.conf"
+NL=$'\n'
 echo && echo -e "$INFO Get trackers ..."
-aria2_conf="/config/aria2.conf"
+ARIA2_CONF="/config/aria2.conf"
 
-tracker=$(wget -qO- https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection/all.txt | awk NF | sed ":a;N;s/\n/,/g;ta")
-[ -z $tracker ] && echo -e "
-$ERROR Unable to get trackers, network failure or invalid links." && exit 1
-echo -e "
---------------------[TRACKERS]--------------------
-${tracker}
---------------------[TRACKERS]--------------------
+DATE_TIME() {
+    date +"%Y/%m/%d %H:%M:%S"
+}
+
+
+GET_TRACKERS() {
+    if [[ -z "${CTU}" ]]; then
+        echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers..."
+        TRACKER=$(
+            ${DOWNLOADER} https://trackerslist.com/all_aria2.txt ||
+                ${DOWNLOADER} https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection@master/all_aria2.txt ||
+                ${DOWNLOADER} https://trackers.p3terx.com/all_aria2.txt
+        )
+    else
+        echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers from url(s):${CTU} ..."
+        URLS=$(echo ${CTU} | tr "," "$NL")
+        for URL in $URLS; do
+            TRACKER+="$(${DOWNLOADER} ${URL} | tr "," "\n")$NL"
+        done
+        TRACKER="$(echo "$TRACKER" | awk NF | sort -u | sed 'H;1h;$!d;x;y/\n/,/' )"
+    fi
+
+    [[ -z "${TRACKER}" ]] && {
+        echo
+        echo -e "$(DATE_TIME) ${ERROR} Unable to get trackers, network failure or invalid links." && exit 1
+    }
+}
+
+ECHO_TRACKERS() {
+    echo -e "
+--------------------[BitTorrent Trackers]--------------------
+${TRACKER}
+--------------------[BitTorrent Trackers]--------------------
 "
-[ ${aria2_conf} == "cat" ] && exit 0
-echo -e "$INFO Adding trackers to '${aria2_conf}' ..." && echo
-if [ ! -f ${aria2_conf} ]; then
-    echo -e "$ERROR '${aria2_conf}' does not exist."
-    exit 1
-else
-    [ -z $(grep "bt-tracker=" ${aria2_conf}) ] && echo "bt-tracker=" >>${aria2_conf}
-    sed -i "s@^\(bt-tracker=\).*@\1${tracker}@" ${aria2_conf} && echo -e "$INFO Trackers added successfully!"
-fi
+}
+
+
+ADD_TRACKERS() {
+    echo -e "$(DATE_TIME) ${INFO} 添加 BT trackers 到 Aria2 配置文件中 ${LIGHT_PURPLE_FONT_PREFIX}${ARIA2_CONF}${FONT_COLOR_SUFFIX} ..." && echo
+    if [ ! -f ${ARIA2_CONF} ]; then
+        echo -e "$(DATE_TIME) ${ERROR} '${ARIA2_CONF}' 不存在"
+        exit 1
+    else
+        [ -z $(grep "bt-tracker=" ${ARIA2_CONF}) ] && echo "bt-tracker=" >>${ARIA2_CONF}
+        sed -i "s@^\(bt-tracker=\).*@\1${TRACKER}@" ${ARIA2_CONF} && echo -e "$(DATE_TIME) ${INFO} 成功添加 BT trackers 到 Aria2 配置文件中!"
+    fi
+}
+
+GET_TRACKERS
+ECHO_TRACKERS
+ADD_TRACKERS
