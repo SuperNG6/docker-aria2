83853bb754817de13a51e70171acd96ff11ec028
SuperNG6 <sleele@gmail.com>
2026-05-24 16:00:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# 从本仓库最新 release 下载对应架构的 aria2c 静态二进制并安装到 /usr/local/bin
-# 通过 GitHub /releases/latest/download/ 自动跟随最新发布，无需手工升级版本号
+# 从本仓库固定 release 下载对应架构的 aria2c 静态二进制并安装到 /usr/local/bin
+# 固定到 ci-20260521-134555-UTC，避免上游 latest 变动影响复现
 # Asset 命名约定：aria2-static-linux-<arch>.tar.gz
 
 INFO="\033[1;32m[INFO]\033[0m"
@@ -17,7 +17,7 @@ case "$(uname -m)" in
         ;;
 esac
 
-URL="https://github.com/SuperNG6/docker-aria2/releases/latest/download/${ASSET}"
+URL="https://github.com/SuperNG6/docker-aria2/releases/download/ci-20260521-134555-UTC/${ASSET}"
 echo -e "${INFO} 下载 ${ASSET}"
 echo -e "${INFO} 来源 ${URL}"
 
